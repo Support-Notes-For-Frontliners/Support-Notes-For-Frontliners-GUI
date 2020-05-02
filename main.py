b164ac447cce7f0cad8590a4191a56ee1f393798
seiyaK1803 <skozakai03@gmail.com>
2020-05-01 21:51:22 -0700

    checkbuttons now autosize, added sender names

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import tkinter
+import tkinter as tk
 import scrollable_frame
 import note_actions
 import textwrap
-tk = tkinter
 
-print('hello world')  # yeah, this is a symbolic blessing. don't remove this.
-print(str(note_actions.get_note_amt(note_actions.data)))
+
+print(str(note_actions.get_note_amt(note_actions.data)) + ' notes received')
+
+"""
+creating main window and defining necessary variables
+"""
 window = tk.Tk()
-window.geometry("300x300+300+300")  # widthxheight (px)
+window.title('Support For Frontliners GUI')
+window.geometry('300x300+300+300')  # widthxheight (px)
 window.vars = []
 data_iterable = note_actions.get_note_condition(
     note_actions.data, 'approved', False)
@@ -35,22 +39,25 @@ def button_callback():
     for data_piece in data_iterable:
         var = tk.StringVar(value=data_piece)
         window.vars.append(var)
-        text_string = str(data_iterable[data_piece])
+        data_string = str(data_iterable[data_piece])
         start_string = '\'note\': '
         end_string = ', \'sender\': '
-        text_string = text_string[text_string.index(
-            start_string) + len(start_string):text_string.index(end_string)]
-        cb = tk.Checkbutton(checkbox_pane.interior, var=var, text=textwrap.fill(text_string, 110),
+        sender_string = textwrap.fill(data_string[data_string.index(
+            end_string) + len(end_string):data_string.index(', \'sent\': ')], 110)
+        note_string = textwrap.fill(data_string[data_string.index(
+            start_string) + len(start_string):data_string.index(end_string)], 110)
+        cb = tk.Checkbutton(checkbox_pane.interior, var=var, text=note_string + '\n' + sender_string,
                             onvalue=data_piece, offvalue="",
-                            anchor="w", width=100, height=8,
-                            relief="flat", highlightthickness=0
+                            anchor='w', width=100, height=len(note_string.split('\n')) + 3,
+                            relief='flat', highlightthickness=0
                             )
         cb.grid(row=x, column=0)
         # cb.deselect()
 
         x += 1
 
-    window.geometry("740x600+300+300")
+    window.geometry('740x600+300+300')
+
 
 # function of save process
 
@@ -65,14 +72,14 @@ def save_command():
 create scrollable frame
 """
 checkbox_pane = scrollable_frame.ScrollableFrame(window, bg='#FFFFFF')
-checkbox_pane.pack(expand="true", fill="both")
+checkbox_pane.pack(expand='true', fill='both')
 
 
 btn_checkbox = tk.Button(checkbox_pane.interior,
-                         text="List: click here to display all unapproved notes", command=button_callback).grid(row=0, column=0)
+                         text='List: click here to display all unapproved notes', command=button_callback).grid(row=0, column=0)
 
 btn_save = tk.Button(checkbox_pane.interior,
-                     text="Save: click here to approve all selected notes", command=save_command).grid(row=1, column=0)
+                     text='Save: click here to approve all selected notes', command=save_command).grid(row=1, column=0)
 
 
 """
